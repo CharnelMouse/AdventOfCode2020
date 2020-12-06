fba9155ed05c0a5b6d86930120b02259b1be7f90
CharnelMouse <markwebster204@yahoo.co.uk>
2020-12-06 09:53:08 +0000

    Account for length-one missing seats at front/back in day 5

--- 5.r
+++ 5.r
@@ -3,5 +3,5 @@ bin <- matrix(as.integer(x == "B" | x == "R"), nrow = nrow(x))
 ids <- colSums(bin * 2^((nrow(x) - 1):0))
 max(ids) # part one
 present <- rle(is.element(0:(2^nrow(x) - 1), ids))
-index <- which(present$lengths == 1 & present$values == FALSE)
+index <- 1L + which(present$lengths[-1] == 1 & present$values[-1] == FALSE)
 sum(present$lengths[1:index]) - 1 # part two
