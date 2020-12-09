32d4c3a5bc717fbb6deca3f05207d87d02beebb7
CharnelMouse <markwebster204@yahoo.co.uk>
2020-12-09 23:32:20 +0000

    Improve day 9 speed

--- 9.r
+++ 9.r
@@ -1,19 +1,25 @@
+# The brief version of this takes so long that I just give the quick version.
 x <- as.numeric(readLines("9.txt")) # later numbers too large for R ints
-find_nonsum <- function(remaining, visited, sums) {
-  # forming the sums with combn for each iteration is
-  # briefer, but takes such a long time that I don't
-  # think it's worth splitting into brief/quick scripts for
+second <- function(n) cumsum(c(n - 1, if (n > 2) 23:(26 - n)))
+by_second <- unlist(lapply(2:25, second)) # Sum order by second index first
+rem_ind <- cumsum(c(1, 1:23)) # Sums involving first precedent
+find_nonsum <- function(remaining, precedents, sums) {
   if (length(remaining) == 0)
     return(NA_real_)
   n <- remaining[1]
   if (!is.element(n, sums))
-    remaining[1]
+    n
   else
-    find_nonsum(remaining[-1], c(visited, n), c(sums, visited + n))
+    find_nonsum(
+      remaining[-1],
+      c(precedents[-1], n),
+      c(sums[-rem_ind], precedents[-1] + n)
+    )
 }
 start <- x[1:25]
-sums <- colSums(combn(start, 2))
-n <- find_nonsum(x[-(1:25)], start, sums)
+# Arrange sums in order of second index, since this is order in which added later
+sums_start <- colSums(combn(start, 2))[by_second]
+n <- find_nonsum(x[-(1:25)], start, sums_start)
 n # part one: 41682220
 worm <- function(total, vec, start, end, s) {
   if (s == total)
@@ -23,8 +29,6 @@ worm <- function(total, vec, start, end, s) {
   else
     worm(total, vec, start + 1L, end, s - vec[start])
 }
-find <- function(total, vec) {
-  worm(total, vec, 1L, 1L, vec[1])
-}
+find <- function(total, vec) worm(total, vec, 1L, 1L, vec[1])
 vec <- find(n, x)
 min(vec) + max(vec) # part two: 5388976
