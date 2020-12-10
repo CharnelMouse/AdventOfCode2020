3e8839a45a162ef4018c2cf41f841d815cb6dccd
CharnelMouse <markwebster204@yahoo.co.uk>
2020-12-10 14:15:55 +0000

    Improve day 10

--- 10.r
+++ 10.r
@@ -1,12 +1,18 @@
-x <- sort(as.integer(readLines("10.txt")))
-prod(table(diff(c(0, x, max(x) + 3)))[c("1", "3")]) # part one: 2812
+x <- sort.int(as.integer(readLines("10.txt")))
+diffs <- diff(c(0, x, max(x) + 3))
+sum(diffs == 1)*sum(diffs == 3) # part one: 2812
 facc <- function(queue, previous_sums, predecessors) {
+  # tried using diffs instead of values, but it was slower
   if (length(queue) == 0)
     return(previous_sums[1])
   val <- queue[1]
   use <- predecessors - val <= 3
   new_sum <- sum(previous_sums[use])
-  facc(queue[-1], c(new_sum, previous_sums[use[-3]]), c(val, predecessors[use[-3]]))
+  facc(
+    queue[-1],
+    c(new_sum, previous_sums[use[-3]]),
+    c(val, predecessors[use[-3]])
+  )
 }
 y <- rev(x)
 res <- facc(c(y[-1], 0), 1, y[1])
