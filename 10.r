ca171994d0a78119259566216bdae002a2be4123
CharnelMouse <markwebster204@yahoo.co.uk>
2020-12-10 10:55:07 +0000

    Improve day 10 speed

--- 10.r
+++ 10.r
@@ -1,15 +1,13 @@
 x <- sort(as.integer(readLines("10.txt")))
 prod(table(diff(c(0, x, max(x) + 3)))[c("1", "3")]) # part one: 2812
-sum_successors <- function(value, values, sums) {
-  sum(sums[values - value <= 3])
+facc <- function(queue, previous_sums, predecessors) {
+  if (length(queue) == 0)
+    return(previous_sums[1])
+  val <- queue[1]
+  use <- predecessors - val <= 3
+  new_sum <- sum(previous_sums[use])
+  facc(queue[-1], c(new_sum, previous_sums[use[-3]]), c(val, predecessors[use[-3]]))
 }
-acc <- function(values, current_sums, index) {
-  if (index > length(values))
-    return(current_sums[index - 1])
-  val <- values[index]
-  poss_inds <- (index - 3:1)[index - 3:1 >= 1]
-  new_sum <- sum_successors(val, values[poss_inds], current_sums[poss_inds])
-  acc(values, c(current_sums, new_sum), index + 1L)
-}
-res <- acc(c(rev(x), 0), 1L, 2L)
+y <- rev(x)
+res <- facc(c(y[-1], 0), 1, y[1])
 format(res, scientific = FALSE) # part two: 386869246296064
