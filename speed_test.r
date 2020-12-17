9f1daa4874dcf330574618979522d847f5565dd4
CharnelMouse <markwebster204@yahoo.co.uk>
2020-12-17 22:45:48 +0000

    Add some speed test utility functions

--- speed_test.r
+++ speed_test.r
@@ -1,4 +1,4 @@
-microbenchmark::microbenchmark(
+times <- microbenchmark::microbenchmark(
   source("1-short.r"),
   source("1-quick.r"),
   source("2.r"),
@@ -20,3 +20,17 @@ microbenchmark::microbenchmark(
   source("17.r"),
   setup = expression(rm(list = ls()))
 )
+
+time_summary <- function(times, scale = 1) {
+  data.table::as.data.table(times)[
+    ,
+    .(
+      mean = mean(time/scale),
+      sd = sd(time/scale),
+      se = sd(time/scale)/.N,
+      `low (2.5%)` = quantile(time/scale, 0.025),
+      `high (97.5%)` = quantile(time/scale, 0.975)
+    ),
+    by = expr
+    ]
+}
