41755d9cb17ca7c8c4bc63ce8d21f1283e314630
CharnelMouse <markwebster204@yahoo.co.uk>
2020-12-19 20:06:56 +0000

    Improve 19.1

--- 19.r
+++ 19.r
@@ -10,54 +10,51 @@ sort_rules <- function(rules) {
 }
 sorted_rules <- sort_rules(rules)
 
-complete <- function(intchars, rules) {
-  sections <- lapply(strtoi(intchars), function(n) rules[[n + 1]])
+check_along_references <- function(str, rule_components, rules) {
+  reference_indices <- strtoi(rule_components)
   Reduce(
-    function(x, y) {
-      as.character(outer(x, y, paste, sep = ""))
+    function(str, index) {
+      if (all(str == ""))
+        return(character())
+      rem <- checkacc(str, index, rules)
+      rem[!is.na(rem)]
     },
-    sections
+    reference_indices,
+    init = str
   )
 }
-
-iterate <- function(rules, dependencies, completed) {
-  if (isTRUE(completed[1]))
-    return(rules[[1]])
-  completeable <- vapply(
-    dependencies,
-    function(ints) all(completed[ints + 1]),
-    logical(1)
-  )
-  todo <- completeable & !completed
-  res <- lapply(
-    strsplit(as.character(rules)[todo], " ", fixed = TRUE),
-    function(chars) {
-      mid <- match("|", chars)
-      if (is.na(mid)) {
-        complete(chars, rules)
-      }else
-        c(
-          complete(chars[1:(mid - 1)], rules),
-          complete(chars[(mid + 1):length(chars)], rules)
-        )
-    }
-  )
-  new_rules <- `[<-`(rules, todo, res)
-  iterate(new_rules, dependencies, completed | todo)
-}
-resolve <- function(rules) {
-  tidy_rules <- gsub("\"", "", rules, fixed = TRUE)
-  completed <- is.element(tidy_rules, c("a", "b"))
-  dependencies <- lapply(
-    strsplit(tidy_rules, " ", fixed = TRUE),
-    function(chars) strtoi(
-      setdiff(
-        chars,
-        c("|", "a", "b")
+checkacc <- function(str, index, rules) {
+  if (all(nchar(str) == 0))
+    return(NA)
+  rule <- rules[index + 1]
+  if (rule == "\"a\"") {
+    rem <- ifelse(substr(str, 1, 1) == "a", substring(str, 2), NA)
+    return(rem[!is.na(rem)])
+  }
+  if (rule == "\"b\"") {
+    rem <- ifelse(substr(str, 1, 1) == "b", substring(str, 2), NA)
+    return(rem[!is.na(rem)])
+  }
+  rule_components <- strsplit(rule, " ", fixed = TRUE)[[1]]
+  mid <- which(rule_components == "|")
+  if (length(mid) == 0) {
+    check_along_references(str, rule_components, rules)
+  }else{
+    starts <- c(1, mid + 1)
+    ends <- c(mid - 1, length(rule_components))
+    rem <- unlist(
+      Map(
+        function(s, e) check_along_references(str, rule_components[s:e], rules),
+        starts,
+        ends
       )
     )
-  )
-  iterate(as.list(tidy_rules), dependencies, completed)
+    rem[!is.na(rem)]
+  }
 }
-possible <- resolve(sorted_rules)
-sum(is.element(messages, possible)) # part one: 144
+check <- function(str, rules) {
+  any(checkacc(str, 0, rules) == "")
+}
+
+possible <- vapply(messages, check, logical(1), sorted_rules)
+sum(possible) # part one: 144
