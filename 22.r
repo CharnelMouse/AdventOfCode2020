8a726e82fbeb704f5b2ab3c0412726b00c95eb1d
CharnelMouse <markwebster204@yahoo.co.uk>
2020-12-26 00:58:27 +0000

    Cut day 22 running time down to two minutes

--- 22.r
+++ 22.r
@@ -1,35 +1,47 @@
-# warning: this takes about 15 mins to run on my input
 x <- readLines("22.txt")
 brk <- which(x == "")
 p1_deck <- strtoi(x[2:(brk - 1)])
 p2_deck <- strtoi(x[(brk + 2):length(x)])
-game <- function(p1_deck, p2_deck, depth, recurse) {
-  since <- 0
+game <- function(p1_deck, p2_deck, depth, recurse, top) {
   p1 <- p1_deck
   p2 <- p2_deck
   len1 <- length(p1)
   len2 <- length(p2)
-  history <- list()
+  p1_history <- list()
+  p2_history <- list()
+  p1_lenhist <- integer()
+  p2_lenhist <- integer()
   while (len1 > 0 && len2 > 0) {
-    if (any(vapply(
-      history,
-      identical,
-      logical(1),
-      list(p1, p2)
-    ))) {
+    lenhist_matches <- p1_lenhist == len1 & p2_lenhist == len2
+    p1_histmatch <- vapply(
+      p1_history[lenhist_matches],
+      function(x) all(x == p1),
+      logical(1)
+    )
+    if (
+      any(p1_histmatch) &&
+      any(vapply(
+        p2_history[lenhist_matches][p1_histmatch],
+        function(x) all(x == p2),
+        logical(1)
+      ))
+    ) {
       len2 <- 0
       break
     }
-    history <- c(history, list(list(p1, p2)))
+    p1_history <- c(p1_history, list(p1))
+    p2_history <- c(p2_history, list(p2))
+    p1_lenhist <- c(p1_lenhist, len1)
+    p2_lenhist <- c(p2_lenhist, len2)
     if (recurse && len1 > p1[1] && len2 > p2[1]) {
       sub_result <- game(
         p1[1 + 1:p1[1]],
         p2[1 + 1:p2[1]],
         depth + 1,
-        recurse
+        recurse,
+        FALSE
       )
-      since <- length(history)
-      p1_wins_round <- sub_result[[1]] == 1
+      p1_wins_round <- sub_result == 1
     }else{
       p1_wins_round <- p1[1] > p2[1]
     }
@@ -46,13 +58,20 @@ game <- function(p1_deck, p2_deck, depth, recurse) {
     len1 <- length(p1)
     len2 <- length(p2)
   }
-  if (len2 == 0)
-    list(1L, p1)
-  else
-    list(2L, p2)
+  if (top) {
+    if (len2 == 0)
+      p1
+    else
+      p2
+  }else{
+    if (len2 == 0)
+      1L
+    else
+      2L
+  }
 }
 play <- function(p1, p2, recurse) {
-  game(p1, p2, 1, recurse)
+  game(p1, p2, 1, recurse, TRUE)
 }
 score <- function(deck) {
   len <- length(deck)
@@ -60,6 +79,6 @@ score <- function(deck) {
 }
 
 res <- play(p1_deck, p2_deck, FALSE)
-score(res[[2]]) # part one: 34255
+score(res) # part one: 34255
 res2 <- play(p1_deck, p2_deck, TRUE)
-score(res2[[2]]) # part two: 33369
+score(res2) # part two: 33369
